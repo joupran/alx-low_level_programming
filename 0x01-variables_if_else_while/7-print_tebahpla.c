9762ba9fef1db8b1c7db1c2b4457294141590867
joupran <a.e.jabri95@gmail.com>
2023-03-17 13:00:50 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 122; i >= 98; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
