89c41b8c35fbb11edbbf4ec81dcade8a7a8823dd
joupran <a.e.jabri95@gmail.com>
2023-03-17 13:18:23 -0700

    number and alphabet

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+int n;
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+for (n = 97; n < 103; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
