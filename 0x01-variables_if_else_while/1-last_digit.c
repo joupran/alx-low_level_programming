d57f0320fdfdf3b96a71c9373086fe908372203c
joupran <a.e.jabri95@gmail.com>
2023-03-17 11:22:55 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ srand(time(0));
 	ld = n % 10;
 	if (ld > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", nn ld);
+	printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
 	else if (ld == 0)
 	{
