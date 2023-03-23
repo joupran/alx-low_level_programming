488e6b727d6c6b1bcdbf7ad3f72cf132514d11e7
joupran <a.e.jabri95@gmail.com>
2023-03-23 13:01:52 -0700

    FIZZBUZZ

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,20 +7,29 @@
  */
 int main(void)
 {
-for (int i = 1; i <= 100; i++)
+int x;
+for (x = 1; x <= 100; x++)
 {
-if (i % 3 == 0)
-printf("Fizz");
-else if (i % 5 == 0)
-printf("Buzz");
-else if (i % 3 == 0 && i % 5 == 0)
+if ((x % 3 == 0) && (x % 5 == 0))
+{
 printf("FizzBuzz");
-else
-printf("%d ", i);
 }
-if (i != 100)
+else if (x % 3 == 0)
+{
+printf("Fizz");
+}
+else if (x % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", x);
+}
+if (x != 100)
 {
 printf(" ");
+}
 }
 printf("\n");
 return (0);
