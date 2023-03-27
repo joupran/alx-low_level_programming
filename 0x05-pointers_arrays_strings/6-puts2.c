fc18fcfbd51f5f469455fc7f1e14a1ed0a55eb94
joupran <a.e.jabri95@gmail.com>
2023-03-27 12:25:31 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
+
 /**
  * puts2 - prints characters in a string
  * @str: string
  * Return: nothing
  */
+
 void puts2(char *str)
 {
-int len;
-int i = 0;
-while (str[i] != '\0')
-{
-len++;
-}
-for (i = 0; i < len; i += 2)
-{
-putchar(str[i]);
-}
-putchar('\n');
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
 }
