4cee4be818d6574b353e2a9a4fbbd914c0b0e4cc
joupran <a.e.jabri95@gmail.com>
2023-04-30 08:23:13 -0700

    print int list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - check your code
+ * @h: parametre
+ *
+ * Return: num
+ */
+size_t print_listint(const listint_t *h)
+{
+
+	size_t a = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		a++;
+		h = h->next;
+	}
+	return (a);
+}
