dce70ef790b27ca0f2a375ebf062479e9793ac43
joupran <a.e.jabri95@gmail.com>
2023-05-04 06:22:43 -0700

    convert binary num

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,21 +7,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int result = 0;
-int i, len;
+	int i;
+	unsigned int decimal_value = 0;
 
-if (b == NULL)
-	return (0);
-for (len = 0; b[len] != '\0'; len++);
-for (i = 0; i < len; i++)
-{
-	if (b[i] == '0')
-		result = (result << 1) | 0;
-	else if (b[i] == '1')
-		result = (result << 1) | 1;
-	else
+	if (!b)
 		return (0);
-}
-return (result);
-}
 
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		decimal_value = 2 * decimal_value + (b[i] - '0');
+	}
+
+	return (decimal_value);
+}
